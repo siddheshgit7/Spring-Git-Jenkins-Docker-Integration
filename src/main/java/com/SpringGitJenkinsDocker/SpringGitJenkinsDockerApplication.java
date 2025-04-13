5fe434127f6831402f94e1af9aaa2600be381190
siddhesh shelke <siddheshshelke86@gmail.com>
2025-04-13 15:41:13 +0530

    docker file added

--- src/main/java/com/SpringGitJenkinsDocker/SpringGitJenkinsDockerApplication.java
+++ src/main/java/com/SpringGitJenkinsDocker/SpringGitJenkinsDockerApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringGitJenkinsDockerApplication {
 
+	private void messaging() {
+		// TODO Auto-generated method stub
+
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGitJenkinsDockerApplication.class, args);
 	}
