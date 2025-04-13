3d6dbc270534bbd699c16a3ce5b52c596ae56455
siddhesh shelke <siddheshshelke86@gmail.com>
2025-04-13 12:45:53 +0530

    first commit

---
+++ src/main/java/com/SpringGitJenkinsDocker/SpringGitJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.SpringGitJenkinsDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitJenkinsDockerApplication.class, args);
+	}
+
+}
